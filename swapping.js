01cc9ae500e3e54dba7b267a9d2783db9aeb3d50
Lawrence-Evans <lawrenceevans309@gmail.com>
2024-04-15 15:13:33 +0100

    Pushed correct palindrome file plus input html.

--- swapping.js
+++ swapping.js
@@ -12,20 +12,17 @@ let length = num.length;
 let secondDigit = numArray.at(1);
 let lastDigit = numArray.at(-1);
 
-// Displaying relevant variables in console
-console.log("num length = " + length);
-console.log("Second digit = " + secondDigit);
-console.log("Last digit = " + lastDigit);
-console.log("Original number = " + num);
+// Displaying relevant variables as multiline alert
+alert("Num length = " + length + "\nSecond digit = " + secondDigit + "\nLast digit = " + lastDigit);
 
 // Looping through numArray. If the second digit is reached it is spliced with the lastDigit 
 // and the last digit spliced with the secondDigit only if the "if" is false and "else if" is true
-// then the array join into a string for display.
+// then the array join into a string and displayed as an alert where both numbers are easily compared.
 for (i=0; i <= length; i++) {
     if (i == 1) {
         numArray.splice(i, 1, lastDigit);
     } else if (i == (length - 1)) {
         numArray.splice((i), 1, secondDigit);
-        console.log("Swapped number = " + numArray.join(""));
-    }
+        alert("Original number   = " + num + "\nSwapped number = " + numArray.join(""));
+    };
 };
