6871cd586536a2bf826bff55721a55b113756452
Lawrence-Evans <lawrenceevans309@gmail.com>
2024-04-23 19:55:30 +0100

    Resumbitted Task 9 and passed.

--- callBack.js
+++ callBack.js
@@ -2,11 +2,22 @@
 let intervalID;
 let counter = 0;
 
-// Variable containing a function that console logs
+// Original submission IGNORE. Variable containing a function that console logs
 // an incremnting counter every second
+// let startCounter = function() {
+//     intervalID = setInterval(function () {
+//         counter ++, console.log(counter)}, 1000);
+// };
+
+// This variable now excepts a callback instead of the increment and print code
+// being in the same block. 
 let startCounter = function() {
-    intervalID = setInterval(function () {
-        counter ++, console.log(counter)}, 1000);
+    intervalID = setInterval(callback, 1000);
+};
+
+// Callback function that increments the counter and prints in to console.
+function callback() {
+    counter ++, console.log(counter);
 };
 
 // A function to clear the intervalID and stop it running code.
@@ -18,15 +29,14 @@ function stopCounter() {
 let startButton = document.getElementById('start');
 let stopButton = document.getElementById('stop');
 
-// Event listener to run timer functions
+// Event listener to call timer functions
 startButton.addEventListener("click", startCounter);
 stopButton.addEventListener("click", stopCounter);
 
-// I was badly confused by the starting code from startCounter being a variable 
-// with a function inside. As I tried to follow the examples on W3 and mdn I 
-// kept getting the syntax wrong as I muddled having a function nested in a function.
-// I am not convinced I have completed the task elegantly. The rest was straight forwared.
+// Okay I know this task is named callBack so I should have used one. But in all
+// honesty as the instructions didn't prompt me to use one I carried on and did it
+// without one initially causing more work for everyone.
 
 // I noticed that startCounter can be called multiple times speeding up the count, is
-// this becaues there are now multiple instances of intervalID? It can not be stopped
+// this becaues there are now multiple instances of intervalID in that window? It can not be stopped
 // with stopCounter in the same way however. Is there a common fix for this?
--- higherOrder.js
+++ higherOrder.js
@@ -1,4 +1,12 @@
-// Declaring an array with Nutmeg, Willow and Bridge as the only three 6 letter words.
+// I have now declared a function as a parameter for
+// myFilterFunction so that it will use the one given and no other.
+// Unlike before where I accessed it directly from outside the function.
+// Thanks for pointing that out.
+
+// Initially I made the lengthCheck function a variable as I thought
+// this was neccessary so it could be parsed as a variable don't know way I thought that.
+
+// Declaring and printing an array with Nutmeg, Willow and Bridge as the only three 6 letter words.
 let wordArray = [
   "Dolphin",
   "Sand",
@@ -13,28 +21,73 @@ let wordArray = [
 ];
 console.log(wordArray);
 
-// My filter function interates through the array with the callback to check if
-// each word is 6 letters. If callback returns true the word is pushed to a new array.
-function myFilterFunction() {
+// My filter function expects an array and funcyion. Interates through any array with any function it is given as a callback.
+// If callback returns true the word is pushed to a new array then returns the filtered array.
+function myFilterFunction(array, fn) {
   let filteredArray = [];
-  for (word of wordArray) {
-    if (myCallback(wordArray) == true) {
+  for (word of array) {
+    if (fn()) {
       filteredArray.push(word);
     }
   }
-  console.log(filteredArray);
+  return filteredArray;
 }
 
-// Callback that checks if a word is 6 letters and returns true.
-let myCallback = function lengthCheck() {
+// Callback that returns true checks if a word is 6 letters.
+function lengthCheck() {
   if (word.length == 6) {
     return true;
   }
-};
+}
 
-// Calling the function with the callback.
-myFilterFunction(wordArray, myCallback);
+// Callback that returns true checks if a word is 4 letters.
+function lengthCheckFour() {
+  if (word.length == 4) {
+    return true;
+  }
+}
 
-// // Built in JS filter function to compare
-// let FilterFunctionResult = wordArray.filter((word) => word.length == 6);
-// console.log(FilterFunctionResult);
+// Calling the function with the callback. Storing it in a variable and printing.
+// Checking that myFilterFunction can parse different functions.
+const sixLetterArarry = myFilterFunction(wordArray, lengthCheck);
+console.log(sixLetterArarry);
+
+const fourLetterArarry = myFilterFunction(wordArray, lengthCheckFour);
+console.log(fourLetterArarry);
+
+// // Earlier code verion that checks if the return (specifically for word) from the callback is true before pushing to a new array.
+
+// // My filter function interates through the array with any function it is given as a callback.
+// // If callback returns true the word is pushed to a new array.
+// function myFilterFunction(wordArray, fn) {
+//   let filteredArray = [];
+//   for (word of wordArray) {
+//     if (fn(wordArray) == true) {
+//       filteredArray.push(word);
+//     }
+//   }
+//   return filteredArray;
+// }
+
+// // Callback that checks if a word is 6 letters and returns true.
+// function lengthCheckSix() {
+//   if (word.length == 6) {
+//     return true;
+//   }
+// };
+
+// // Calling the function with the callback. Storing it in a variable and printing.
+// const sixLetterArarry = myFilterFunction(wordArray, lengthCheckSix);
+// console.log(sixLetterArarry)
+
+// // Checking that myFilterFunction can parse different functions.
+// // Callback that checks if a word is 4 letters and returns true.
+// function lengthCheckFour() {
+//   if (word.length == 4) {
+//     return true;
+//   }
+// };
+
+// // Calling the function with the callback. Storing it in a variable and printing.
+// const fourLetterArarry = myFilterFunction(wordArray, lengthCheckFour);
+// console.log(fourLetterArarry)
